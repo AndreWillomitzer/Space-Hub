37edc0e03d6c63d60df16eabe47ef4f440986d09
Andre Willomitzer <andre.willomitzer@gmail.com>
2021-08-04 12:01:20 -0400

    Added some age functions to Astronaughtcomponent

--- src/app/astronaughts/astronaughts.component.ts
+++ src/app/astronaughts/astronaughts.component.ts
@@ -8,13 +8,34 @@ import { AstronaughtDataService } from '../astronaught-data.service';
 })
 export class AstronaughtsComponent implements OnInit {
   astronaughtArray: any;
+  astronautAgeArray : any;
   constructor(private asDataService:AstronaughtDataService) { }
-
+  getAgePart(str) : string {
+    var astroDate = str.split('-')[0];
+    var today = new Date();
+    var year = today.getFullYear();
+    var astroYearOB = astroDate.getFullYear();
+    console.log("Age: ",(year - astroYearOB).toString());
+    return (year - astroYearOB).toString();
+  }
+/*   getAllAges(str) : void{
+    for(var i = 0; i < this.astronaughtArray.length; i++){
+      var currentAge = this.getAgePart((this.astronaughtArray.results[i].date_of_birth).toString());
+      console.log(`Age: ${currentAge}`);
+      this.astronautAgeArray[i] = currentAge;      
+    }
+  } */
   ngOnInit(): void {
     this.asDataService.getAstronaughts().subscribe(data=>{
       this.astronaughtArray = data;
       console.log("Astronaught data: ", this.astronaughtArray);
-      //this.astronaughtArray.next();
+/*       for(var i = 0; i < this.astronaughtArray.length; i++){
+        this.astronautAgeArray[i] = this.getAgePart((this.astronaughtArray.results[i].date_of_birth).toString());
+        console.log(this.astronautAgeArray[i]);
+      } */
+      for(var astronaught of this.astronaughtArray.results){
+        console.log(this.getAgePart((astronaught.date_of_birth).toString()));
+      }      
     });
   }
 
