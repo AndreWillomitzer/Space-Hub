77d265ed1552c3e83fc6b683907dfc54d0b16549
Kevan-Y <58233223+Kevan-Y@users.noreply.github.com>
2021-09-03 12:27:14 -0400

    Add responsive to navbar and hero section

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, HostListener } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -6,16 +7,34 @@ import { Component, OnInit, HostListener } from '@angular/core';
   styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent implements OnInit {
-  constructor() {}
   isScrolled = false;
+  isActive = false;
+  constructor(private router: Router) {
+    router.events.subscribe((val) => {
+      if (val instanceof NavigationEnd) this.isActive = false;
+    });
+    const mediaListener = window.matchMedia('(max-width: 1024px)');
+    if (mediaListener.addEventListener)
+      mediaListener.addEventListener(`change`, () => {
+        this.isActive = false;
+      });
+    // Deprecated 'MediaQueryList' API, <Safari 14, IE, <Edge 16
+    else
+      mediaListener.addListener(() => {
+        this.isActive = false;
+      });
+  }
 
   ngOnInit(): void {}
 
   @HostListener('window:scroll')
   scrollEvent() {
-    console.log(window.pageYOffset, this.isScrolled);
-    window.pageYOffset >= 500
+    window.pageYOffset >= 250
       ? (this.isScrolled = true)
       : (this.isScrolled = false);
+  }
+
+  clickMenu() {
+    this.isActive = this.isActive ? false : true;
   }
 }
